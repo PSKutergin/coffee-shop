42a6dde740b54897244bb645f736dae905d8ebfe
PSKutergin <PSKutergin@gmail.com>
2023-10-27 09:45:01 +0300

    add nav bar, products and footer components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Глобальное подключение стилей
+import './assets/scss/style.scss'
+
+// Глобальная регистрация компонента
+// import FirstComponent from './components/FirstComponent.vue' 
+
 Vue.config.productionTip = false
+
+// Глобальная регистрация компонента
+// Vue.component('first-component', FirstComponent)
 
 new Vue({
   render: h => h(App),
