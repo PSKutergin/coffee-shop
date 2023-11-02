51921c34b8564ab9ca70a93e4836fab97e61f541
PSKutergin <PSKutergin@gmail.com>
2023-11-02 08:33:57 +0300

    add nav item

---
+++ src/components/NavItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <li :class="classLink">
+        <router-link :to="link">{{ text }}</router-link>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: {
+            link: {
+                require: true,
+                type: String,
+            },
+            text: {
+                require: false,
+                type: String,
+            },
+            classLink: {
+                require: true,
+                type: String,
+            },
+        }
+    }
+</script>
